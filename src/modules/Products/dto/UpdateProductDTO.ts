6b325b1120be1609b4441e707af01738ab07ac21
Maria Alice <marialice2003@gmail.com>
2024-04-01 18:09:47 -0300

    feat: update and delete product

---
+++ src/modules/Products/dto/UpdateProductDTO.ts
@@ -0,0 +1,19 @@
+import { PricingGroup, Category } from "@prisma/client";
+
+interface IUpdateProductDTO {
+  id: string;
+  artist: string;
+  album: string;
+  year: string;
+  producer: string;
+  numberOfTracks: string;
+  height: string;
+  width: string;
+  weight: string;
+  pricingGroup: PricingGroup;
+  categories: Category[];
+  barCode: string;
+  price: string;
+}
+
+export { IUpdateProductDTO }
---
+++ src/modules/Products/services/DeleteProductService.ts
@@ -0,0 +1,21 @@
+import { inject, injectable } from "tsyringe";
+import { IProductRepository } from "../repositories/ProductRepositoryInterface";
+
+@injectable()
+class DeleteProductService {
+  constructor(
+    @inject('ProductRepository')
+    private productRepository: IProductRepository
+  ) {}
+
+async execute(id: string): Promise<void> {
+    const product = await this.productRepository.findById(id);
+
+    if(!product) {
+        throw new Error('Produto não encontrado');
+    }
+        await this.productRepository.delete(product);
+    }
+}
+
+export { DeleteProductService };

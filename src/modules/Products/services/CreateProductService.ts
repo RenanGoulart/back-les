6840eb03b4feec8635c96493966711f0bf5d573a
Maria Alice <marialice2003@gmail.com>
2024-05-13 18:31:27 -0300

    fix: generate bar code on products

--- src/modules/Products/services/CreateProductService.ts
+++ src/modules/Products/services/CreateProductService.ts
@@ -2,7 +2,7 @@ import { inject, injectable } from "tsyringe";
 import { Product } from "../entities/Product";
 import { IProductRepository } from "../repositories/ProductRepositoryInterface";
 import { ICreateProductDTO } from "../dto/ProductDTO";
-
+import { randomBytes } from "crypto";
 @injectable()
 class CreateProductService {
   constructor(
@@ -11,9 +11,20 @@ class CreateProductService {
   ) {}
 
   async execute(data: ICreateProductDTO): Promise<Product> {
-    const product = await this.productRepository.create(data);
+    const barcode = generateBarcode();
+    const productData = {
+      ...data,
+      barCode: barcode,
+    };
+
+    const product = await this.productRepository.create(productData);
     return product;
   }
 }
 
+function generateBarcode(): string {
+  return Math.floor(1000000000000 + Math.random() * 9000000000000).toString();
+}
+
 export { CreateProductService };
+

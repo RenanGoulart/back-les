0bf353141fd988f156357e555eb9da7f59ed50a9
Renan Goulart <renan.goulart4@gmail.com>
2024-05-26 22:40:45 -0300

    fix: validation when create coupon

--- src/modules/Coupon/services/CreateCouponService.ts
+++ src/modules/Coupon/services/CreateCouponService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from "tsyringe";
 import { Coupon } from "../entities/Coupon";
 import { ICouponRepository } from "../repositories/CouponRepositoryInterface";
 import { ICreateCouponDTO } from "../dto/CouponDTO";
+import { BadRequestError } from "../../../shared/helpers/apiErrors";
 
 @injectable()
 class CreateCouponService {
@@ -11,6 +12,12 @@ class CreateCouponService {
   ) {}
 
   async execute(data: ICreateCouponDTO): Promise<Coupon> {
+    const couponExists = await this.couponRepository.findByName(data.name);
+
+    if (couponExists && couponExists.expirationDate > new Date()) {
+      throw new BadRequestError('Coupon já existe');
+    }
+
     const coupon = await this.couponRepository.create(data);
 
     return coupon;
